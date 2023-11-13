6193f08829e95942823adecb13cc5ca4099b9080
MostafaAlaa297 <elaskary609@gmail.com>
2023-11-13 10:29:37 -0800

    Add shell program

---
+++ shell.c
@@ -0,0 +1,60 @@
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sus/wait.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * shell - A shell interface to execute
+ * different commands
+ */
+
+int shell()
+{
+	struct path_node *path_list = build_path_list();
+	char *input = NULL;
+	size_t len = 0;
+	ssize_t n;
+	int i;
+
+	while (1)
+	{
+		_putchar('$');
+		_putchar(' ');
+		fflush(stdout);
+		n = getline(&input, &len, stdin);
+		if (n == -1)
+		{
+			if (feof(stdin))
+			{
+				_putchar('\n');
+				_putchar('^');
+				_putchar('D');
+				_putchar('\n');
+				break;
+			}
+			else
+			{
+				perror("getline");
+				break;
+			}
+		}
+		i = 0;
+		while (input[i])
+		{
+			_putchar(input[i]);
+			i++;
+		}
+		execute_command(input, path_list);
+		free(input);
+		input = NULL;
+		len = 0;
+	}
+	free(input);
+	free_paths(path_list);
+	
+	return (0);
+}
