f9a8879a8312073c1f262968d6784621a7ce8e95
Khalafawy <mohamedkhalaf11@gmail.com>
2023-11-15 10:28:56 -0800

    _getenv

---
+++ _getenv.c
@@ -0,0 +1,24 @@
+
+/**
+ * _getenv - gets an environment variable
+ * and returns its value.
+ * @name: the name of the variable.
+ *
+ * Return: a pointer to its string value.
+ */
+
+char *_getenv(const char *name)
+{
+	extern char **environ;
+	unsigned int i = 0;
+	char *token;
+
+	while (environ[i] != NULL)
+	{
+		token = strtok(environ[i], "=");
+		if (_strcmp(token, name) == 0)
+			return (strtok(NULL, "="));
+		i++;
+	}
+	return (NULL);
+}
