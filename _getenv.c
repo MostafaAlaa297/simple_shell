91c2f4fca390dfb57a2e1670be504d42bcc33f95
Khalafawy <mohamedkhalaf11@gmail.com>
2023-11-15 17:17:33 -0800

    _getenv

--- _getenv.c
+++ _getenv.c
@@ -1,7 +1,5 @@
-/*#include <string.h>
-#include <stddef.h>*/
-#include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
 #include "main.h"
 
 /**
@@ -16,34 +14,14 @@ char *_getenv(const char *name)
 {
 	extern char **environ;
 	unsigned int i = 0;
-	/*char *token;*/
-	char *path_env = NULL;
+	char *token;
 
-	for (i = 0; environ[i]; i++)
-	{
-		if (_strncmp(environ[i], name, 5) == 0)
-		{
-			path_env = environ[i] + 5;
-			break;
-		}
-	}
-
-	if (path_env == NULL)
-	{
-		printf("PATH env not found\n");
-		return (NULL);
-	}
-
-	printf("%s\n", path_env);
-	return (path_env);
-
-
-	/*while (environ[i] != NULL)
+	while (environ[i] != NULL)
 	{
 		token = strtok(environ[i], "=");
 		if (_strcmp(token, name) == 0)
 			return (strtok(NULL, "="));
 		i++;
 	}
-	return (NULL);*/
+	return (NULL);
 }
